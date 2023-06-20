abdc32a0d33251da3ad22938042ec93d6176e417
Roger Pender <rdpender3@gmail.com>
2023-06-20 11:37:25 -0700

    set up state

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
-const messages = ['Work on react projects', 'Work on data algorithms', 'Leverage the new skills'];
+import { useState } from 'react';
+
+const messages = ['Work on react projects', 'Work on data algorithms', 'Leverage my new skills'];
 
 function App() {
-  let step = 1;
+  let [step, setStep] = useState(1);
+
+  function handlePrev() {
+    if (step >= 1) setStep(step--);
+  }
+
+  function handleNext() {
+    if (step <= 3) setStep(step++);
+  }
 
   return (
     <div className="steps">
       <div className="numbers">
-        <div className={`${step >= 1 ? 'active' : ''}`}>1</div>
-        <div className={`${step >= 2 ? 'active' : ''}`}>2</div>
-        <div className={`${step >= 3 ? 'active' : ''}`}>3</div>
+        <div className={step >= 1 ? 'active' : ''}>1</div>
+        <div className={step >= 2 ? 'active' : ''}>2</div>
+        <div className={step >= 3 ? 'active' : ''}>3</div>
       </div>
       <p className="message">
         Step {step}: {messages[step - 1]}
       </p>
       <div className="buttons">
-        <button className="button-purp">Previous</button>
-        <button className="button-purp">Next</button>
+        <button className="button-purp" onClick={handlePrev}>
+          Previous
+        </button>
+        <button className="button-purp" onClick={handleNext}>
+          Next
+        </button>
       </div>
     </div>
   );
