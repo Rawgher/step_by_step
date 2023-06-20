7f13bbc68488508a5403fe54f58c49d051af5c63
Roger Pender <rdpender3@gmail.com>
2023-06-20 11:09:21 -0700

    setting up basic building blocks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+const messages = ['Work on react projects', 'Work on data algorithms', 'Leverage the new skills'];
+
 function App() {
-  return <div>This is my app</div>;
+  let step = 1;
+
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step >= 1 ? 'active' : ''}`}>1</div>
+        <div className={`${step >= 2 ? 'active' : ''}`}>2</div>
+        <div className={`${step >= 3 ? 'active' : ''}`}>3</div>
+      </div>
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
+      <div className="buttons">
+        <button className="button-purp">Previous</button>
+        <button className="button-purp">Next</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
